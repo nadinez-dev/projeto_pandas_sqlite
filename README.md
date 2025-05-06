085d1f48516b27d38f2fd5ec996a19806e758b17
POSITIVO <n4h.owl@gmail.com>
2025-05-06 17:22:23 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto : pandas + SQLite
+
+Este projeto foi criado para treinar o uso do SQLite e da biblioteca 'pandas' no Python.
+
+## Funcionalidades:
+- criação de banco de dados SQLite
+- inserção de dados via Python
+- leitura com pandas
+- exibição de DataFrames
+
+## Requisitos
+- Python 3.x
+- bibliotecas:
+  - sqlite3 (nativa)
+  - pandas ('pip install pandas')
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sqlite3
 import pandas as pd
 
+# conecta ou cria banco
 con = sqlite3.connect('clinica.db')
 
+# deleta a tabela antiga (se já existir)
 con.execute("DROP TABLE IF EXISTS pacientes")
 
+# cria  atabela com a coluna cidade
 con.execute('''
 CREATE TABLE pacientes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,9 +16,12 @@ CREATE TABLE pacientes (
     cidade TEXT
 )
 ''')
+
+# insere dados
 con.execute("INSERT INTO pacientes (nome, idade, cidade) VALUES ('Lucas', 32, 'Curitiba')")
 con.execute("INSERT INTO pacientes (nome, idade, cidade) VALUES ('Renata', 28, 'São Paulo')")
 con.commit()
 
+# le com pandas
 df=pd.read_sql_query("SELECT * FROM pacientes", con)
 print(df)
